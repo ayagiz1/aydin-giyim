a174ec2f284fb728c7c1601c46673ec83f0a42cb
Ahmet Yagiz AYDIN <ahmetyagiizaydin@gmail.com>
2025-08-20 12:15:10 +0300

    fix: mobil gorunumde gorsel hatalar duzeltildi.
    -Site gorunumuyle alakali kucuk degisiklikler yapildi.
    -UI, kullanicin kolayca kavrayabilecegi sekilde gelistirildi.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,15 +7,26 @@ import heroImageFormal from '../assets/hero-formal.jpg';
 const Hero = () => {
     return (
         <div className="hero-container">
-            <img src={heroImageCasual} alt="Gündelik erkek giyim tarzı" className="hero-image" />
-            <img src={heroImageFormal} alt="Resmi erkek giyim tarzı" className="hero-image" />
+            <div className='hero-panel'>
+                <img src={heroImageCasual} alt="Gündelik erkek giyim tarzı" className="hero-image" />
+                <div className="hero-content">
+                    <h1 className="hero-title">Tarzınıza Değer Katıyoruz</h1>
+                    <p className="hero-subtitle">
+                        En trend erkek giyim ürünleri ile stilinizi bir üst seviyeye taşıyın.
+                    </p>
+                    <button className="hero-button">Tarzını Keşfet</button>
+                </div>
+            </div>  
 
-            <div className="hero-content">
-                <h1 className="hero-title">Tarzınıza Değer Katıyoruz</h1>
-                <p className="hero-subtitle">
-                    En trend erkek giyim ürünleri ile stilinizi bir üst seviyeye taşıyın.
-                </p>
-                <button className="hero-button">Koleksiyonu Keşfet</button>
+            <div className='hero-panel'>
+            <img src={heroImageFormal} alt="Resmi erkek giyim tarzı" className="hero-image" />
+                <div className="hero-content">
+                    <h1 className='hero-title'>Özel Günlerde Gözler Üzerinizde Olsun</h1>
+                    <p className='hero-subtitle'>
+                        Şıklığınızı tamamlayacak birbirinden eşsiz takımlarımıza göz atın.
+                    </p>
+                    <button className="hero-button">Tarzını Keşfet</button>
+                </div>
             </div>
         </div>
     );
